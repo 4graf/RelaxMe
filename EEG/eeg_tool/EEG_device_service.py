a7a5be55d2f672deae724df1fa1e3e8208be1c21
4graf <senya.nabatov@mail.ru>
2024-04-07 01:29:22 +0300

    Тестирование онлайн сбора данных ЭЭГ

--- EEG/eeg_tool/EEG_device_service.py
+++ EEG/eeg_tool/EEG_device_service.py
@@ -28,21 +28,37 @@ params.serial_port = 'COM3'  # Укажите порт, к которому по
 board = BoardShim(BoardIds.CYTON_BOARD, params)
 board.prepare_session()
 
+# Получение частоты записи
+sampling_rate = board.get_sampling_rate(BoardIds.CYTON_BOARD)
+print('Частота записи:', sampling_rate, 'Гц')
+
+# Получение имен каналов
+channel_names = board.get_eeg_names(BoardIds.CYTON_BOARD)
+print('Имена каналов:', channel_names)
+
 # Начало сбора данных
 board.start_stream()
 
-data = []
-start_time = time.time()
-print(start_time)
-# Получение данных в режиме онлайн
-while time.time() - start_time < 1:
-    record = board.get_current_board_data(256)  # Получение 256 отсчетов данных
-    # Обработка данных здесь
-    data.append(record)
-print(time.time())
-print(data[-1].shape)
+duration_in_seconds = 5
+# data = board.get_current_board_data(int(sampling_rate * duration_in_seconds))
+time.sleep(5)
+data = board.get_current_board_data(250*5)
+
+# data = []
+# start_time = time.time()
+# print(start_time)
+# # Получение данных в режиме онлайн
+# while time.time() - start_time < 1:
+#     record = board.get_current_board_data(250)  # Получение 250 отсчетов данных
+#     # Обработка данных здесь
+#     print(record)
+#     data.append(record)
+# print(time.time())
+# print(data[-1].shape)
+print(data.shape)
 with open(r"C:\Users\Арсений\Desktop\records.txt", mode='w') as f:
-    f.write(str(data[-1].tolist()))
+    # f.write(str(data[-1].tolist()))
+    f.write(str(data.tolist()))
 
 # Завершение сеанса сбора данных
 board.stop_stream()
