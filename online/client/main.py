9ec329e672e608340f8e5174a4c0e84ae1f538f5
4graf <senya.nabatov@mail.ru>
2024-04-22 22:47:01 +0300

    Построение графиков ч2

--- online/client/main.py
+++ online/client/main.py
@@ -15,7 +15,7 @@ import numpy as np
 
 eeg_device_service = EEGDeviceService()
 
-# stress_recognizer_service = StressRecognizerService()
+stress_recognizer_service = StressRecognizerService()
 
 
 async def do_predict():
@@ -34,25 +34,35 @@ async def do_predict():
 
     fig.legend(labels=eeg_device_service.channel_names,
                loc="upper right")
-
+    predictions = []
     plt.ion()
     plt.show(block=False)
+
+    x = np.arange(0, 60, 1 / eeg_device_service.sampling_rate).tolist()  # ToDo: добавить динамическую коррекцию х оси
+
     for _ in range(1, 25):
         # time.sleep(5)
         # await asyncio.sleep(5)
-        # data = eeg_device_service.get_data(only_eeg=True)
 
         plt.pause(1)
         data = eeg_device_service.get_data(only_eeg=True)
+
+        # prediction = await stress_recognizer_service.predict_stress(data, DataMode.RAW)
+        # async with AsyncClient() as client:
+        #     response = await client.post('http://localhost:3000/api/v1/stress/predict', json={'data': data.tolist()})
+        #     prediction = response.json()
+        # print(prediction)
+        prediction = [1]
+
         new_data_plot = data.tolist()
-        print(eeg_device_service.sampling_rate)
-        x.extend([x[-1] + (i / eeg_device_service.sampling_rate) for i in range(1, len(new_data_plot[0])+1)])
-        print(x[-1])
+        # x.extend([x[-1] + (i / eeg_device_service.sampling_rate) for i in range(1, len(new_data_plot[0])+1)])
         for all_data_plot, new_data in zip(data_plot, new_data_plot):
             all_data_plot.extend(new_data)
 
         for ax, line, data in zip(axs, lines, data_plot):  # ToDo: Замерить время. При необходимости оптимизировать zip
-            line.set_data(x, data)
+            line.set_data(x[:len(data)], data)
+
+            # ax.axvspan(x[-1]-1, x[-1], facecolor='r', alpha=0.2)
 
             ax.relim()
             ax.autoscale_view()
@@ -60,22 +70,13 @@ async def do_predict():
             ax.draw_artist(ax.patch)
             ax.draw_artist(line)
 
+
+
+
         fig.canvas.update()
         fig.canvas.flush_events()
         # plt.pause(1)
 
-    # for _ in range(10):
-    #     await asyncio.sleep(1)
-    #     # data = eeg_device_service.get_data(only_eeg=True)
-    #     data = np.random.rand(2, 10)
-    #     new_data_plot = data.copy().tolist()[:2]
-    #     print(anim.new_frame_seq())
-
-        # prediction = await stress_recognizer_service.predict_stress(data, DataMode.RAW)
-        # async with AsyncClient() as client:
-        #     response = await client.post('http://localhost:3000/api/v1/stress/predict', json={'data': data.tolist()})
-        #     prediction = response.json()
-        # print(prediction)
 
 asyncio.run(do_predict())
 
